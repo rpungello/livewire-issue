e88f31b1f06d3adcb2ee27f0009cbfd0d1ce72a5
Rob Pungello <rob@pdfsystems.com>
2025-08-20 11:38:29 -0400

    Remove unnecessary stuff

--- routes/web.php
+++ routes/web.php
@@ -1,13 +1,7 @@
 <?php
 
-use App\Livewire\Settings\Appearance;
-use App\Livewire\Settings\Password;
-use App\Livewire\Settings\Profile;
+use App\Livewire\ParentComponent;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-})->name('home');
-
-Route::get('/parent', \App\Livewire\ParentComponent::class)->name('parent');
+Route::get('/', ParentComponent::class)->name('parent');
 
